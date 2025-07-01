613f0c190f65297fbaf417193fbf42241da0e276
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2025-07-01 16:28:53 +0530

    updated resume

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const sections = document.querySelectorAll('section');
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('header nav');
 
-
-
 menuIcon.addEventListener('click', ()=> {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
